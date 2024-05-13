445e56644ddf2fa9fb4be11a284fbf2cd8f74c23
Mike <566936@edu.rocmn.nl>
2024-05-13 10:58:52 +0200

    styling fix

--- assets/pages/Gebruikersdashboard.php
+++ assets/pages/Gebruikersdashboard.php
@@ -11,7 +11,7 @@ if (isset($_POST['logout'])) {
     session_destroy();
 
     // Redirect to index.php
-    header("Location: /index.php");
+    header("Location: index.php");
     exit;
 }
 ?>
@@ -21,7 +21,7 @@ if (isset($_POST['logout'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Dashboard</title>
-    <link rel="stylesheet" href="/css/style.css"> <!-- Include your style.css -->
+    <link rel="stylesheet" href="css/style.css"> <!-- Include your style.css -->
 </head>
 <body>
     <h1>Welcome to User Dashboard</h1>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <title>Gezondheidsmeter</title>
 </head>
@@ -13,7 +13,7 @@
     </header>
     <nav>
         <ul>
-            <li><a href="/index.php">Home</a></li>
+            <li><a href="index.php">Home</a></li>
             <li><a href="/assets/pages/inlogpagina.php">Login</a></li>
             <li><a href="/assets/pages/registreerpagina.php">Register</a></li>
         </ul>
